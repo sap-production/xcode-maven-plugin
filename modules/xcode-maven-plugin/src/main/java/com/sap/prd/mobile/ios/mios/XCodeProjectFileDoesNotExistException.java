b07a90d0a64bfecab08151836bba2472f1ea6a1a
Marcus Holl <marcus.holl@sap.com>
2012-07-26 16:08:58 +0200

    Improve error message from XCodeProjectValidator

---
+++ modules/xcode-maven-plugin/src/main/java/com/sap/prd/mobile/ios/mios/XCodeProjectFileDoesNotExistException.java
@@ -0,0 +1,30 @@
+package com.sap.prd.mobile.ios.mios;
+
+public class XCodeProjectFileDoesNotExistException extends XCodeValidationException
+{
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 3037528457964020084L;
+
+  public XCodeProjectFileDoesNotExistException()
+  {
+    super();
+  }
+
+  public XCodeProjectFileDoesNotExistException(String message, Throwable cause)
+  {
+    super(message, cause);
+  }
+
+  public XCodeProjectFileDoesNotExistException(String message)
+  {
+    super(message);
+  }
+
+  public XCodeProjectFileDoesNotExistException(Throwable cause)
+  {
+    super(cause);
+  }
+}
---
+++ modules/xcode-maven-plugin/src/main/java/com/sap/prd/mobile/ios/mios/XCodeRootFolderDoesNotExistException.java
@@ -0,0 +1,30 @@
+package com.sap.prd.mobile.ios.mios;
+
+public class XCodeRootFolderDoesNotExistException extends XCodeValidationException
+{
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -5981363259390365451L;
+
+  public XCodeRootFolderDoesNotExistException()
+  {
+    super();
+  }
+
+  public XCodeRootFolderDoesNotExistException(String message, Throwable cause)
+  {
+    super(message, cause);
+  }
+
+  public XCodeRootFolderDoesNotExistException(String message)
+  {
+    super(message);
+  }
+
+  public XCodeRootFolderDoesNotExistException(Throwable cause)
+  {
+    super(cause);
+  }
+}
