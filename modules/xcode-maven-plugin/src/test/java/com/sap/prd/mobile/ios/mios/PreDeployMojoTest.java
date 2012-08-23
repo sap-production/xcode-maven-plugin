09a8f8dcbedc685c2e74784c1eeaef3abe5b0646
Marcus Holl <marcus.holl@sap.com>
2012-08-23 10:27:54 +0200

    Add unit tests for final artifacts

--- modules/xcode-maven-plugin/src/test/java/com/sap/prd/mobile/ios/mios/PreDeployMojoTest.java
+++ modules/xcode-maven-plugin/src/test/java/com/sap/prd/mobile/ios/mios/PreDeployMojoTest.java
@@ -40,39 +40,87 @@ public class PreDeployMojoTest
   }
 
   @Test
+  public void testGetRedirectHtmlFilename_UnknownForFinalArtifact() {
+    assertEquals("MyApp-MyApp-1.0.0-Unknown.zip.htm", getRedirectHtmlFilename("MyApp-1.0.0-Unknown.zip", "MyApp"));
+  }
+
+  
+  @Test
   public void testGetRedirectHtmlFilename_AppStoreMetaData() {
     assertEquals("MyApp-AppStoreMetaData.zip.htm", getRedirectHtmlFilename("MyApp-1.0.0-20120821.132955-1-AppStoreMetaData.zip", "MyApp"));
   }
 
+  @Test
+  public void testGetRedirectHtmlFilename_AppStoreMetaDataForFinalArtifact() {
+    assertEquals("MyApp-AppStoreMetaData.zip.htm", getRedirectHtmlFilename("MyApp-1.0.0-AppStoreMetaData.zip", "MyApp"));
+  }
+
+  
   @Test
   public void testGetRedirectHtmlFilename_OtaHtm() {
     assertEquals("MyApp-Release-iphoneos-ota.htm", getRedirectHtmlFilename("MyApp-1.0.0-20120821.132955-1-Release-iphoneos-ota.htm", "MyApp"));
   }
 
   @Test
+  public void testGetRedirectHtmlFilename_OtaHtmForFinalArtifact() {
+    assertEquals("MyApp-Release-iphoneos-ota.htm", getRedirectHtmlFilename("MyApp-1.0.0-Release-iphoneos-ota.htm", "MyApp"));
+  }
+
+  
+  @Test
   public void testGetRedirectHtmlFilename_dSym() {
     assertEquals("MyApp-Release-iphoneos-app.dSYM.zip.htm", getRedirectHtmlFilename("MyApp-1.0.0-20120821.132955-1-Release-iphoneos-app.dSYM.zip", "MyApp"));
   }
 
+  @Test
+  public void testGetRedirectHtmlFilename_dSymForFinalArtifact() {
+    assertEquals("MyApp-Release-iphoneos-app.dSYM.zip.htm", getRedirectHtmlFilename("MyApp-1.0.0-Release-iphoneos-app.dSYM.zip", "MyApp"));
+  }
+
+  
   @Test
   public void testGetRedirectHtmlFilename_appZip() {
     assertEquals("MyApp-Release-iphoneos-app.zip.htm", getRedirectHtmlFilename("MyApp-1.0.0-20120821.132955-1-Release-iphoneos-app.zip", "MyApp"));
   }
 
   @Test
+  public void testGetRedirectHtmlFilename_appZipForFinalArtifact() {
+    assertEquals("MyApp-Release-iphoneos-app.zip.htm", getRedirectHtmlFilename("MyApp-1.0.0-Release-iphoneos-app.zip", "MyApp"));
+  }
+
+  
+  @Test
   public void testGetRedirectHtmlFilename_Ipa() {
     assertEquals("MyApp-Release-iphoneos.ipa.htm", getRedirectHtmlFilename("MyApp-1.0.0-20120821.132955-1-Release-iphoneos.ipa", "MyApp"));
   }
 
+  @Test
+  public void testGetRedirectHtmlFilename_IpaForFinalArtifact() {
+    assertEquals("MyApp-Release-iphoneos.ipa.htm", getRedirectHtmlFilename("MyApp-1.0.0-Release-iphoneos.ipa", "MyApp"));
+  }
+
+  
   @Test
   public void testGetRedirectHtmlFilename_versionsXml() {
     assertEquals("MyApp-versions.xml.htm", getRedirectHtmlFilename("MyApp-1.0.0-20120821.132955-1-versions.xml", "MyApp"));
   }
 
   @Test
+  public void testGetRedirectHtmlFilename_versionsXmlForFinalArtifact() {
+    assertEquals("MyApp-versions.xml.htm", getRedirectHtmlFilename("MyApp-1.0.0-versions.xml", "MyApp"));
+  }
+
+  
+  @Test
   public void testGetRedirectHtmlFilename_Pom() {
     assertEquals("MyApp.pom.htm", getRedirectHtmlFilename("MyApp-1.0.0-20120821.132955-1.pom", "MyApp"));
   }
+
+  @Test
+  public void testGetRedirectHtmlFilenameForFinalArtifact_Pom() {
+    assertEquals("MyApp.pom.htm", getRedirectHtmlFilename("MyApp-1.0.0.pom", "MyApp"));
+  }
+
   
   @Test
   public void testGetRedirectHtmlFilename_mavenMetadata() {
