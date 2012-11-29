1718f0c5e0d12eb0c604323e7375119d9fe955ee
Marcus Holl <marcus.holl@sap.com>
2012-11-29 14:27:39 +0100

    Reduce visibiliy of EffectiveSettings.

--- modules/xcode-maven-plugin/src/main/java/com/sap/prd/mobile/ios/mios/EffectiveBuildSettings.java
+++ modules/xcode-maven-plugin/src/main/java/com/sap/prd/mobile/ios/mios/EffectiveBuildSettings.java
@@ -30,20 +30,20 @@ import java.util.Properties;
 
 import org.apache.commons.io.IOUtils;
 
-public class EffectiveBuildSettings
+class EffectiveBuildSettings
 {
-  public static final String PRODUCT_NAME = "PRODUCT_NAME";
-  public static final String SRC_ROOT = "SRCROOT";
-  public static final String GCC_GENERATE_DEBUGGING_SYMBOLS = "GCC_GENERATE_DEBUGGING_SYMBOLS";
-  public static final String CODE_SIGN_IDENTITY = "CODE_SIGN_IDENTITY";
-  public static final String CODESIGNING_FOLDER_PATH = "CODESIGNING_FOLDER_PATH";
-  public static final String INFOPLIST_FILE = "INFOPLIST_FILE";
-  public static final String PUBLIC_HEADERS_FOLDER_PATH = "PUBLIC_HEADERS_FOLDER_PATH";
-  public static final String BUILT_PRODUCTS_DIR = "BUILT_PRODUCTS_DIR";
+  static final String PRODUCT_NAME = "PRODUCT_NAME";
+  static final String SRC_ROOT = "SRCROOT";
+  static final String GCC_GENERATE_DEBUGGING_SYMBOLS = "GCC_GENERATE_DEBUGGING_SYMBOLS";
+  static final String CODE_SIGN_IDENTITY = "CODE_SIGN_IDENTITY";
+  static final String CODESIGNING_FOLDER_PATH = "CODESIGNING_FOLDER_PATH";
+  static final String INFOPLIST_FILE = "INFOPLIST_FILE";
+  static final String PUBLIC_HEADERS_FOLDER_PATH = "PUBLIC_HEADERS_FOLDER_PATH";
+  static final String BUILT_PRODUCTS_DIR = "BUILT_PRODUCTS_DIR";
   
   private final static Map<Key, Properties> buildSettings = new HashMap<Key, Properties>();
   
-  public static String getBuildSetting(XCodeContext context, String configuration, String sdk, String key) throws XCodeException
+  static String getBuildSetting(XCodeContext context, String configuration, String sdk, String key) throws XCodeException
   {
     return getBuildSettings(context, configuration, sdk).getProperty(key);
   }
