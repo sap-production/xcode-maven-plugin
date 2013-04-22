7b3882dc7b801549cc644ede6946b3e04b53d26c
Irina Kirilova <irina.kirilova@sap.com>
2013-04-22 10:26:05 +0200

    Define perform inside PackagingTypeAction enum.

--- modules/xcode-maven-plugin/src/main/java/com/sap/prd/mobile/ios/mios/PackagingTypeAction.java
+++ modules/xcode-maven-plugin/src/main/java/com/sap/prd/mobile/ios/mios/PackagingTypeAction.java
@@ -20,7 +20,72 @@
 
 package com.sap.prd.mobile.ios.mios;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.archiver.ArchiverException;
+import org.codehaus.plexus.archiver.UnArchiver;
+import org.codehaus.plexus.archiver.manager.ArchiverManager;
+import org.codehaus.plexus.archiver.manager.NoSuchArchiverException;
+
 public enum PackagingTypeAction
 {
-  UNPACK, COPY, BUNDLE;
+  UNPACK() {
+
+    public void perform(ArchiverManager archiverManager, MavenProject project, Artifact mainArtifact)
+    {
+      final File f = FolderLayout.getFolderForExtractedAdditionlUnpackedArtifactsWithGA(project, mainArtifact.getGroupId(), mainArtifact.getArtifactId());
+      if(!f.exists() && !f.mkdirs())
+        throw new IllegalStateException("Cannot create directory " + f);
+      
+      final File unpackMe = mainArtifact.getFile();
+      unarchive(archiverManager, "zip", unpackMe, f);
+    }
+  },
+  COPY() {
+    public void perform(ArchiverManager archiverManager, MavenProject project, Artifact mainArtifact) throws IOException
+    {
+      final File f = FolderLayout.getFolderForCopiedAdditionlUnpackedArtifactsWithGA(project, mainArtifact.getGroupId(), mainArtifact.getArtifactId());
+      if(!f.exists() && !f.mkdirs())
+        throw new IllegalStateException("Cannot create directory " + f);
+
+      final File copyMe = mainArtifact.getFile();
+      FileUtils.copyFile(copyMe, new File(f, copyMe.getName()));
+    }
+  }
+  ,
+  BUNDLE() {
+    public void perform(ArchiverManager archiverManager, MavenProject project, Artifact mainArtifact)
+    {
+      final File f = new File(FolderLayout.getFolderForAdditionlBundlesWithGA(project, mainArtifact.getGroupId(), mainArtifact.getArtifactId()), mainArtifact.getArtifactId() + ".bundle");
+      if(!f.exists() && !f.mkdirs()) {
+        throw new IllegalStateException("Cannot create directory " + f);
+      }
+      final File unpackMe = mainArtifact.getFile();
+      unarchive(archiverManager, "zip", unpackMe, f);
+    }
+  };
+
+  public abstract void perform(ArchiverManager archiverManager, MavenProject project, Artifact mainArtifact) throws IOException;
+
+  private static void unarchive(ArchiverManager archiverManager, final String archiverType, final File source, final File destinationDirectory)
+  {
+    try {
+      UnArchiver unarchiver = archiverManager.getUnArchiver(archiverType);
+      unarchiver.setSourceFile(source);
+      unarchiver.setDestDirectory(destinationDirectory);
+      unarchiver.extract();
+    }
+    catch (NoSuchArchiverException e) {
+      throw new RuntimeException(e);
+    }
+    catch (ArchiverException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
+
+
