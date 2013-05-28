1c035b4aa3f81addf763a706f470ecae60887d03
Marcus Holl <marcus.holl@sap.com>
2013-05-28 13:58:30 +0200

    Provide more integration tests for the verification infrastructure.

--- modules/it-xcode-maven-plugin/src/test/java/com/sap/prd/mobile/ios/mios/XCodeValidationChecksTest.java
+++ modules/it-xcode-maven-plugin/src/test/java/com/sap/prd/mobile/ios/mios/XCodeValidationChecksTest.java
@@ -23,16 +23,157 @@ import static junit.framework.Assert.fail;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
 import org.junit.Test;
 
 public class XCodeValidationChecksTest extends XCodeTest
 {
+  @Test
+  public void testVerifyWithError() throws Exception
+  {
+    final String dynamicVersion = "1.0." + String.valueOf(System.currentTimeMillis());
+    final String testName = getTestName();
+
+    final File remoteRepositoryDirectory = getRemoteRepositoryDirectory(getClass().getName());
+
+    prepareRemoteRepository(remoteRepositoryDirectory);
+
+    final File checkDefinitions = new File(getTestExecutionDirectory(testName, "MyApp"), "checkDefinitions.xml");
+    
+    Properties pomReplacements = new Properties();
+    pomReplacements.setProperty(PROP_NAME_DEPLOY_REPO_DIR, remoteRepositoryDirectory.getAbsolutePath());
+    pomReplacements.setProperty(PROP_NAME_DYNAMIC_VERSION, dynamicVersion);
+
+    Map<String, String> additionalSystemProperties = new HashMap<String, String>();
+    additionalSystemProperties.put("xcode.verification.checks.skip", "false");
+    additionalSystemProperties.put("xcode.verification.checks.definitionFile", "file:" + checkDefinitions.getAbsolutePath());
+
+    test(testName, new File(getTestRootDirectory(), "straight-forward/MyLibrary"),
+          "deploy",
+          THE_EMPTY_LIST,
+          null, pomReplacements, new NullProjectModifier());
+    
+    Verifier v = new Verifier(getTestExecutionDirectory(testName, "MyApp").getAbsolutePath());
+    
+    try
+    {
+        test(v, testName, new File(getTestRootDirectory(), "straight-forward/MyApp"),
+              "verify",
+              THE_EMPTY_LIST,
+              additionalSystemProperties, pomReplacements, new ProjectModifier() {
+
+                @Override
+                void execute() throws Exception
+                {
+                  prepareCheckDefinitionFile(new File(".",
+                        "src/test/resources/checkDefinitions.xml"), checkDefinitions, "ERROR");
+                }
+          
+        });
+        fail();
+    } catch(VerificationException ex) {
+      v.verifyTextInLog("Verification check 'com.sap.prd.mobile.ios.mios.TestMetadataCheck failed. 7430190670433136460");
+    }
+  }
+
+  @Test
+  public void testVerifyMissingCheckDefinitionFile() throws Exception
+  {
+    final String dynamicVersion = "1.0." + String.valueOf(System.currentTimeMillis());
+    final String testName = getTestName();
+
+    final File remoteRepositoryDirectory = getRemoteRepositoryDirectory(getClass().getName());
+
+    prepareRemoteRepository(remoteRepositoryDirectory);
+
+    final File checkDefinitions = new File(getTestExecutionDirectory(testName, "MyApp"), "checkDefinitions.xml");
+    
+    Properties pomReplacements = new Properties();
+    pomReplacements.setProperty(PROP_NAME_DEPLOY_REPO_DIR, remoteRepositoryDirectory.getAbsolutePath());
+    pomReplacements.setProperty(PROP_NAME_DYNAMIC_VERSION, dynamicVersion);
+
+    Map<String, String> additionalSystemProperties = new HashMap<String, String>();
+    additionalSystemProperties.put("xcode.verification.checks.skip", "false");
+    additionalSystemProperties.put("xcode.verification.checks.definitionFile", "file:" + checkDefinitions.getAbsolutePath());
+
+    test(testName, new File(getTestRootDirectory(), "straight-forward/MyLibrary"),
+          "deploy",
+          THE_EMPTY_LIST,
+          null, pomReplacements, new NullProjectModifier());
+    
+    Verifier v = new Verifier(getTestExecutionDirectory(testName, "MyApp").getAbsolutePath());
+    
+    try
+    {
+        test(v, testName, new File(getTestRootDirectory(), "straight-forward/MyApp"),
+              "verify",
+              THE_EMPTY_LIST,
+              additionalSystemProperties, pomReplacements, new NullProjectModifier());
+        fail();
+    } catch(VerificationException ex) {
+      v.verifyTextInLog("Cannot get check definitions");
+    }
+  }
+
+  
+  @Test
+  public void testVerifyWithWarning() throws Exception
+  {
+    final String dynamicVersion = "1.0." + String.valueOf(System.currentTimeMillis());
+    final String testName = getTestName();
+
+    final File remoteRepositoryDirectory = getRemoteRepositoryDirectory(getClass().getName());
+
+    prepareRemoteRepository(remoteRepositoryDirectory);
+
+    final File checkDefinitions = new File(getTestExecutionDirectory(testName, "MyApp"), "checkDefinitions.xml");
+    
+    Properties pomReplacements = new Properties();
+    pomReplacements.setProperty(PROP_NAME_DEPLOY_REPO_DIR, remoteRepositoryDirectory.getAbsolutePath());
+    pomReplacements.setProperty(PROP_NAME_DYNAMIC_VERSION, dynamicVersion);
+
+    Map<String, String> additionalSystemProperties = new HashMap<String, String>();
+    additionalSystemProperties.put("xcode.verification.checks.skip", "false");
+    additionalSystemProperties.put("xcode.verification.checks.definitionFile", "file:" + checkDefinitions.getAbsolutePath());
+
+    test(testName, new File(getTestRootDirectory(), "straight-forward/MyLibrary"),
+          "deploy",
+          THE_EMPTY_LIST,
+          null, pomReplacements, new NullProjectModifier());
+    
+    Verifier v = test(testName, new File(getTestRootDirectory(), "straight-forward/MyApp"),
+              "verify",
+              THE_EMPTY_LIST,
+              additionalSystemProperties, pomReplacements, new ProjectModifier() {
+
+                @Override
+                void execute() throws Exception
+                {
+                  prepareCheckDefinitionFile(new File(".",
+                        "src/test/resources/checkDefinitions.xml"), checkDefinitions, "WARNING");
+                }
+          
+        });
+      v.verifyTextInLog("[WARNING] Verification check 'com.sap.prd.mobile.ios.mios.TestMetadataCheck failed. 7430190670433136460");
+  }
+
+  
+  private void prepareCheckDefinitionFile(File source, File target, String severity) throws IOException
+  {
+   String checks = FileUtils.readFileToString(source);
+   checks = checks.replaceAll("\\$\\{xcode.maven.plugin.version\\}", getMavenXcodePluginVersion());
+   checks = checks.replaceAll("\\$\\{SERVERITY\\}", severity);
+   FileUtils.writeStringToFile(target, checks);
+    
+  }
+
   @Test
   public void testValidationCheckSkipped() throws Exception
   {
