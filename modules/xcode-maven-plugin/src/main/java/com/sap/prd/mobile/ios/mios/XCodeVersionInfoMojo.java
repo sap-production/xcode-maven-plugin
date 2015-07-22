bdc9a041a57691a84d4007f898fe64f9e3cbfc40
Sreedhara GS <sreedhara.g.s@sap.com>
2015-07-22 15:43:44 +0200

    added logs for re-codesigning feature

--- modules/xcode-maven-plugin/src/main/java/com/sap/prd/mobile/ios/mios/XCodeVersionInfoMojo.java
+++ modules/xcode-maven-plugin/src/main/java/com/sap/prd/mobile/ios/mios/XCodeVersionInfoMojo.java
@@ -289,7 +289,7 @@ public class XCodeVersionInfoMojo extends BuildContextAwareMojo
 
           boolean isCodeSignActive = true;
 
-          if((codeSignIdentity == null ||codeSignIdentity.isEmpty()) && !codeSigningRequired )
+          if((codeSignIdentity == null ||codeSignIdentity.isEmpty()) && !codeSigningRequired)
               isCodeSignActive = false;
 
           ExecResult originalCodesignEntitlementsInfo = null;
@@ -300,6 +300,11 @@ public class XCodeVersionInfoMojo extends BuildContextAwareMojo
                originalCodesignEntitlementsInfo = CodeSignManager
                      .getCodesignEntitlementsInformation(appFolder);
                originalSecurityCMSMessageInfo = CodeSignManager.getSecurityCMSInformation(appFolder);
+          }else{
+        	  getLog().info("CODE_SIGNING_REQUIRED=\"NO\"");
+        	  getLog().info("value of codeSignIdentity is "+codeSignIdentity);
+        	  getLog().info("value of codeSigningRequired is "+codeSigningRequired);
+
           }
           try {
             if (hideConfidentialInformation) {
@@ -377,6 +382,7 @@ public class XCodeVersionInfoMojo extends BuildContextAwareMojo
 
   private void sign(File rootDir, String configuration, String sdk) throws IOException, XCodeException
   {
+	
     String csi = EffectiveBuildSettings.getBuildSetting(
           getXCodeContext(XCodeContext.SourceCodeLocation.WORKING_COPY, configuration, sdk),
           EffectiveBuildSettings.CODE_SIGN_IDENTITY);
@@ -384,6 +390,9 @@ public class XCodeVersionInfoMojo extends BuildContextAwareMojo
           getXCodeContext(XCodeContext.SourceCodeLocation.WORKING_COPY, configuration, sdk),
           EffectiveBuildSettings.CODESIGNING_FOLDER_PATH));
     CodeSignManager.sign(csi, appFolder, true);
+	getLog().info("value of codeSignIdentity choosed for resign app:  "+EffectiveBuildSettings.CODE_SIGN_IDENTITY);
+	getLog().info("value of codeSigningRequired during resign "+codeSigningRequired);
+
   }
 
   private List<Dependency> getDependencies() throws IOException
